0e5512a4dfcc581817f6c4473214a9158015558d
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-16 11:05:29 +0100

    Import copy, update styles

--- app/_components/Education.tsx
+++ app/_components/Education.tsx
@@ -1,29 +1,24 @@
 import HorizontalRule from './HorizontalRule';
 
-// import { copy } from '../_library/copy';
+import { copy } from '../_library/copy';
 
 export default function EducationSection() {
 	return (
 		<section className="mb-14">
-			<HorizontalRule colour="purple-800" />
-			<h2 className="section-heading pb-1 section-heading--accent-2 font-bold uppercase text-purple">
-				Education
+			<HorizontalRule colour="typescript" />
+			<h2 className="mb-3 font-bold uppercase text-typescript">
+				{copy.education.sectionTitle}
 			</h2>
-			<ul className="education-list">
-				<li className="mb-5">
-					<h3 className="font-bold text-purple">Codecademy</h3>
-					<p className="italic mb-1">November 2023-Present</p>
-					<p>Full-Stack Engineer Career Path</p>
-				</li>
-
-				<li className="mb-2">
-					<p></p>
-					<h3 className="font-bold text-purple">
-						Trinity College of Music, London
-					</h3>
-					<p className="italic mb-1">2010-2014</p>
-					<p>Bachelor of Music, Cello</p>
-				</li>
+			<ul className="">
+				{copy.education.educationItems.map((educationItem, index) => (
+					<li className="mb-7" key={index}>
+						<h3 className="font-bold text-typescript">
+							{educationItem.institution}
+						</h3>
+						<p className="italic">{educationItem.period}</p>
+						<p>{educationItem.course}</p>
+					</li>
+				))}
 			</ul>
 		</section>
 	);
