7e10543599204aa0f5b713a2f94563f7da3ea96f
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-16 20:15:28 +0100

    Use new Icon component & Tailwind styles

--- app/_components/WorkHistoryItem.tsx
+++ app/_components/WorkHistoryItem.tsx
@@ -1,6 +1,5 @@
-import { ArrowRightCircleIcon } from '@heroicons/react/24/outline';
-
 import { WorkHistoryItem as WorkHistoryItemProps } from '../_types/types';
+import Icon from './Icon';
 
 export default function WorkHistoryItem({
 	title,
@@ -9,15 +8,15 @@ export default function WorkHistoryItem({
 	responsibilities,
 }: WorkHistoryItemProps) {
 	return (
-		<section className="work-history-item">
-			<h3 className={`mt-3 mb-1 font-bold text-green-800`}>{title}</h3>
+		<section className="mb-10">
+			<h3 className={`mt-3 mb-1 font-bold text-typescript`}>{title}</h3>
 			<p>{company}</p>
 			<p className="italic mb-1">{period}</p>
-			<ul className="bullet-list">
+			<ul>
 				{responsibilities.map((responsibility, index) => (
 					<li key={index}>
 						<div className="flex justify-items-start">
-							<ArrowRightCircleIcon className="size-6 flex-shrink-0 text-green-800 mr-2" />
+							<Icon name="arrow" size={32} colour="text-typescript" />
 							<span>{responsibility}</span>
 						</div>
 					</li>
