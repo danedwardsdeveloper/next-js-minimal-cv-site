52779d19a6024d54cc41aaec81991bc2ae8a6c77
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-16 20:13:58 +0100

    Update Tailwind layout

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -4,7 +4,7 @@ import { copy } from '../_library/copy';
 
 export default function Footer() {
 	return (
-		<footer className="col-span-full flex-col-reverse md:flex-row gap-4 md:gap-0 md:row-start-2 flex justify-between md:items-end mb-10">
+		<footer className="w-full col-span-full flex-col-reverse md:flex-row gap-4 md:gap-0 md:row-start-2 flex justify-between md:items-end mb-10">
 			<SimpleAnalyticsBadge />
 			<p className="text-xs">{copy.copyright}</p>
 		</footer>
