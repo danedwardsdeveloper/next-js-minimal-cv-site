16c64188928c8259f65bf7dfd35a4a3022c2906a
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-16 20:14:16 +0100

    Create MenuBar.tsx

---
+++ app/_components/MenuBar.tsx
@@ -0,0 +1,9 @@
+import LightDarkButton from './LightDarkButton';
+
+export default function MenuBar() {
+	return (
+		<div className="mx-6 w-full col-span-full row-start-1 flex justify-end mt-6">
+			<LightDarkButton />
+		</div>
+	);
+}
