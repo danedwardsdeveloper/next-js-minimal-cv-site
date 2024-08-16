f3186d9ddbd4e382b44767fa9035014e687eacf4
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-16 20:15:39 +0100

    Add Tailwind styles

--- app/_components/WorkHistorySection.tsx
+++ app/_components/WorkHistorySection.tsx
@@ -7,8 +7,8 @@ import { WorkHistoryItem as WorkHistoryItemProps } from '../_types/types';
 export default function WorkHistorySection() {
 	return (
 		<section>
-			<HorizontalRule colour="green-800" />
-			<h2 className="section-heading pb-1 font-bold uppercase text-green-800">
+			<HorizontalRule colour="typescript" />
+			<h2 className="pb-1 font-bold uppercase text-typescript">
 				Work history
 			</h2>
 			{copy.workHistory.map(
