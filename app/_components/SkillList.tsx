6e7208629f74e0d5279f09c2af49ff38cc645aa4
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-16 20:15:02 +0100

    Use new Icon component

--- app/_components/SkillList.tsx
+++ app/_components/SkillList.tsx
@@ -1,4 +1,4 @@
-import { CheckCircleIcon } from '@heroicons/react/24/outline';
+import Icon from './Icon';
 
 interface SkillListProps {
 	title: string;
@@ -9,12 +9,13 @@ interface SkillListProps {
 export default function SkillList({ title, skills, colour }: SkillListProps) {
 	return (
 		<section className="mb-14">
-			<h2 className={`pb-1 font-bold uppercase text-${colour}`}>{title}</h2>
+			<h2 className={`mb-3 font-bold uppercase text-${colour}`}>{title}</h2>
+
 			<ul>
 				{skills.map((skill, index) => (
-					<li key={index} className={`mb-2`}>
+					<li key={index} className="mb-3">
 						<div className="flex justify-items-start">
-							<CheckCircleIcon className="size-6 flex-shrink-0 text-purple-800 mr-2" />
+							<Icon name="checkCircle" colour="typescript" />
 							<span>{skill}</span>
 						</div>
 					</li>
