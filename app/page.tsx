4231b513c802f8b3db84b5fbdbb8072a9a8cda16
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-16 10:55:21 +0100

    Fix responsive spacing

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import './_styles/oldStyles.scss';
-
 import ProfessionalSummary from './_components/ProfessionalSummary';
 import PortfolioSection from './_components/PortfolioSection';
 import WorkHistorySection from './_components/WorkHistorySection';
@@ -10,18 +8,18 @@ import Footer from './_components/Footer';
 
 export default function Home() {
 	return (
-		<>
-			<main className="main-column order-2 max-w-550">
+		<div className="grid grid-cols-1 md:grid-cols-[1fr,2fr] gap-6 md:gap-20 mx-6 md:mx-10 lg:mx-20 animate-fade-in">
+			<main className="md:col-start-2 md:col-end-3 mt-8 md:mt-24 flex-grow max-w-[550px]">
 				<ProfessionalSummary />
 				<PortfolioSection />
 				<WorkHistorySection />
 			</main>
-			<aside className="order-1 mt-16">
+			<aside className="md:col-start-1 md:col-end-2 md:row-start-1 mt-8 md:mt-16">
 				<SkillsSection />
 				<EducationSection />
 				<Contact />
 			</aside>
 			<Footer />
-		</>
+		</div>
 	);
 }
