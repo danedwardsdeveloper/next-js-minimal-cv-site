c9a62f1357beb534171d9dab223e465bf59f614c
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-17 07:17:43 +0100

    Make button functional

--- app/_components/LightDarkButton.tsx
+++ app/_components/LightDarkButton.tsx
@@ -1,13 +1,27 @@
+'use client';
+import { useTheme } from '../_contexts/ThemeContext';
+
+import { ThemeOptions } from '../_types/types';
+
 import Icon from './Icon';
 
-const darkModeEnabled: boolean = false;
-const currentIcon = darkModeEnabled ? 'sun' : 'moon';
-
 export default function LightDarkButton() {
+	const { theme, setTheme } = useTheme();
+
+	const toggleTheme = () => {
+		setTheme((prevTheme: ThemeOptions) =>
+			prevTheme === 'light' ? 'dark' : 'light'
+		);
+	};
+
+	const currentIcon = theme === 'dark' ? 'sun' : 'moon';
+
 	return (
 		<button
 			type="button"
-			className=" items-center rounded-md bg-white px-2 py-2  ring-gray-300  border-typescript  border-2 hover:bg-gray-200  hover:text-white transition-colors ease-in-out duration-300 shadow-lg"
+			onClick={toggleTheme}
+			className=" items-center rounded-md bg-white dark:bg-gray-800 px-2 py-2  ring-gray-300  border-typescript  border-2 hover:bg-gray-200 dark:hover:bg-gray-700  hover:text-white transition-colors ease-in-out duration-300 shadow-lg"
+			aria-label={`Switch to ${theme === 'light' ? 'dark' : 'light'} mode`}
 		>
 			<Icon name={currentIcon} padding="p-0" colour="typescript" />
 		</button>
