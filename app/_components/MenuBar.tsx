f7acca8609c52fb73da1fcb35b6ce1d062b43cfe
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-17 07:18:16 +0100

    Change button position

--- app/_components/MenuBar.tsx
+++ app/_components/MenuBar.tsx
@@ -2,7 +2,7 @@ import LightDarkButton from './LightDarkButton';
 
 export default function MenuBar() {
 	return (
-		<div className="mx-6 w-full col-span-full row-start-1 flex justify-end mt-6">
+		<div className="mx-6 col-span-full row-start-1 flex justify-center mt-6 ">
 			<LightDarkButton />
 		</div>
 	);
