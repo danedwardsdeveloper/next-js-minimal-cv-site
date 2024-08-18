f710dc739e16899e32d836037f5d90d476322ad7
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-18 14:07:45 +0300

    Use theme colours, add link hover style

--- app/_components/PortfolioPiece.tsx
+++ app/_components/PortfolioPiece.tsx
@@ -1,9 +1,8 @@
 import Link from 'next/link';
 
-import { PortfolioPiece as PortfolioPieceProps } from '../_types/types';
+import { PortfolioPiece as PortfolioPieceProps } from '../types';
 
 import Icon from './Icon';
-import { globalStyles } from '../_library/themes';
 
 export default function PortfolioPiece({
 	name,
@@ -17,16 +16,16 @@ export default function PortfolioPiece({
 			<h3 className="font-bold uppercase mt-3 mb-1">
 				<Link
 					href={link}
-					className="hover-animation hover-animation--purple text-typescript"
+					className="text-theme-primary hover:text-theme-secondary hover:scale-105 transition-all duration-300"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
 					{name}
 				</Link>
-				<span className="text-gray-400">&nbsp;|&nbsp;</span>
+				<span className="text-theme-secondary px-3">|</span>
 				<Link
 					href={githubLink}
-					className=" text-typescript"
+					className="text-theme-primary hover:text-theme-secondary hover:scale-105 transition-all duration-300"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
@@ -36,9 +35,9 @@ export default function PortfolioPiece({
 			<p className="mb-4">{description}</p>
 			<ul>
 				{details.map((detail, index) => (
-					<li className={globalStyles.listItemMargin} key={index}>
+					<li className="mb-3" key={index}>
 						<div className="flex justify-items-start">
-							<Icon name="arrow" size={32} colour="text-typescript" />
+							<Icon name="arrow" size={32} colour="text-theme-primary" />
 							<span>{detail}</span>
 						</div>
 					</li>
