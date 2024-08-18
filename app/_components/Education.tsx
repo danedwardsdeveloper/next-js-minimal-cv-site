f074f2b4004ffd3d5d5dd713b7f4b0bcee2c008a
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-18 14:05:59 +0300

    Use theme styles

--- app/_components/Education.tsx
+++ app/_components/Education.tsx
@@ -5,14 +5,14 @@ import { copy } from '../_library/copy';
 export default function EducationSection() {
 	return (
 		<section className="mb-14">
-			<HorizontalRule colour="typescript" />
-			<h2 className="mb-3 font-bold uppercase text-typescript">
+			<HorizontalRule />
+			<h2 className="mb-3 font-bold uppercase text-theme-primary">
 				{copy.education.sectionTitle}
 			</h2>
 			<ul className="">
 				{copy.education.educationItems.map((educationItem, index) => (
 					<li className="mb-7" key={index}>
-						<h3 className="font-bold text-typescript">
+						<h3 className="font-bold text-theme-primary">
 							{educationItem.institution}
 						</h3>
 						<p className="italic">{educationItem.period}</p>
