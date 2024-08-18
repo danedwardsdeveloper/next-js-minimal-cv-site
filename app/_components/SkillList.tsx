20081099184abbff7edc41dabd1caf131e29b6a7
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-18 14:11:14 +0300

    Use theme colours

--- app/_components/SkillList.tsx
+++ app/_components/SkillList.tsx
@@ -3,19 +3,20 @@ import Icon from './Icon';
 interface SkillListProps {
 	title: string;
 	skills: string[];
-	colour: string;
 }
 
-export default function SkillList({ title, skills, colour }: SkillListProps) {
+export default function SkillList({ title, skills }: SkillListProps) {
 	return (
 		<section className="mb-14">
-			<h2 className={`mb-3 font-bold uppercase text-${colour}`}>{title}</h2>
+			<h2 className="mb-3 font-bold uppercase text-theme-primary">
+				{title}
+			</h2>
 
 			<ul>
 				{skills.map((skill, index) => (
 					<li key={index} className="mb-3">
 						<div className="flex justify-items-start">
-							<Icon name="checkCircle" colour="typescript" />
+							<Icon name="checkCircle" colour="text-theme-primary" />
 							<span>{skill}</span>
 						</div>
 					</li>
