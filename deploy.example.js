738877a35e76fd222a986030aaa99d654216e9c1
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-18 14:37:41 +0300

    Build site before deploying

--- deploy.example.js
+++ deploy.example.js
@@ -1,7 +1,6 @@
-// deploy.example.ts
 const { execSync } = require('child_process');
 
-const command = `fly deploy \
+const command = `pnpm run build && fly deploy \
   --build-secret SECRET1="your_secret_value_1" \
   --build-secret SECRET2="your_secret_value_2" \
   --build-secret SECRET3="your_secret_value_3"`;
