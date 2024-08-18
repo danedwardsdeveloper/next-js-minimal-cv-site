047067a7f2d5b4fae9467a850d1c40fb9c5500eb
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-18 14:11:58 +0300

    Use theme colours

--- app/_components/WorkHistoryItem.tsx
+++ app/_components/WorkHistoryItem.tsx
@@ -1,4 +1,4 @@
-import { WorkHistoryItem as WorkHistoryItemProps } from '../_types/types';
+import { WorkHistoryItem as WorkHistoryItemProps } from '../types';
 import Icon from './Icon';
 
 export default function WorkHistoryItem({
@@ -9,14 +9,14 @@ export default function WorkHistoryItem({
 }: WorkHistoryItemProps) {
 	return (
 		<section className="mb-10">
-			<h3 className={`mt-3 mb-1 font-bold text-typescript`}>{title}</h3>
+			<h3 className="mt-3 mb-1 font-bold text-theme-primary">{title}</h3>
 			<p>{company}</p>
-			<p className="italic mb-1">{period}</p>
+			<p className="italic mb-3">{period}</p>
 			<ul>
 				{responsibilities.map((responsibility, index) => (
-					<li key={index}>
+					<li key={index} className="mb-3">
 						<div className="flex justify-items-start">
-							<Icon name="arrow" size={32} colour="text-typescript" />
+							<Icon name="arrow" size={32} colour="text-theme-primary" />
 							<span>{responsibility}</span>
 						</div>
 					</li>
