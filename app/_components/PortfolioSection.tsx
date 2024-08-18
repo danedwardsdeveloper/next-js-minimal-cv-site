889b8b4a801d7ebe8bee07b6c33266f027d6f285
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-18 14:07:57 +0300

    Use theme colours

--- app/_components/PortfolioSection.tsx
+++ app/_components/PortfolioSection.tsx
@@ -2,13 +2,15 @@ import { copy } from '../_library/copy';
 import PortfolioPiece from './PortfolioPiece';
 import HorizontalRule from './HorizontalRule';
 
-import { PortfolioPiece as PortfolioPieceProps } from '../_types/types';
+import { PortfolioPiece as PortfolioPieceProps } from '../types';
 
 export default function PortfolioSection() {
 	return (
 		<section className="mb-12">
-			<HorizontalRule colour="typescript" />
-			<h2 className="pb-1 font-bold uppercase text-typescript">Portfolio</h2>
+			<HorizontalRule />
+			<h2 className="pb-1 font-bold uppercase text-theme-primary">
+				Portfolio
+			</h2>
 
 			{copy.portfolio.map(
 				(portfolioPiece: PortfolioPieceProps, index: number) => (
