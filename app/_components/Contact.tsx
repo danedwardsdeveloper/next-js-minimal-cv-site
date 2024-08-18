848f8e8f6a4c727c12732c91ce5d0c8999ca1ca8
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-18 14:05:51 +0300

    Add obfuscated email address, render contact items dynamically

--- app/_components/Contact.tsx
+++ app/_components/Contact.tsx
@@ -1,31 +1,46 @@
 import Link from 'next/link';
 
+import ObfuscatedEmail from './ObfuscatedEmail';
 import HorizontalRule from './HorizontalRule';
 import Icon from './Icon';
 
 import { copy } from '../_library/copy';
-import { globalStyles } from '../_library/themes';
 
 export default function Contact() {
 	return (
 		<section className="mb-10">
-			<HorizontalRule colour="typescript" />
-			<h2 className="section-heading pb-1 font-bold uppercase text-typescript">
+			<HorizontalRule />
+
+			<h2 className="section-heading font-bold uppercase text-theme-primary">
 				{copy.contact.sectionTitle}
 			</h2>
-			<ul className="">
+
+			<ul className="mt-3">
+				{copy.contact.email.display && (
+					<li className="mb-3">
+						<ObfuscatedEmail />
+					</li>
+				)}
+
 				{copy.contact.contactItems.map(
 					(contactItem, index) =>
 						contactItem.display && (
-							<li className={globalStyles.listItemMargin} key={index}>
-								<Link href={contactItem.value}>
+							<li className="mb-3" key={index}>
+								<Link
+									href={contactItem.value}
+									rel="noopener"
+									target="_blank"
+									title={`Open Dan's ${contactItem.name} in a new window`}
+								>
 									<div className="flex justify-items-start items-center">
 										<Icon
-											name="arrow"
-											size={32}
-											colour="text-typescript"
+											name="link"
+											size={24}
+											colour="text-theme-primary"
 										/>
-										<span>{contactItem.name}</span>
+										<span className="text-theme-text hover:text-theme-primary hover:scale-105 transition-all duration-300">
+											{contactItem.name}
+										</span>
 									</div>
 								</Link>
 							</li>
