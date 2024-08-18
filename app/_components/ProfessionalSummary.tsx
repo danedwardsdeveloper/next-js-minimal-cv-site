b4025836fc68a9da93a16a0c9a7f5c81078f8e5a
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-18 14:11:05 +0300

    Style updates, remove prop from HR

--- app/_components/ProfessionalSummary.tsx
+++ app/_components/ProfessionalSummary.tsx
@@ -4,8 +4,8 @@ import { copy } from '../_library/copy';
 
 export default function ProfessionalSummary() {
 	return (
-		<section className="mt-8 md:mt-28 mb-12">
-			<HorizontalRule colour="background-theme-primary" />
+		<section className="mt-6 md:mt-14 lg:mt-28 mb-12">
+			<HorizontalRule />
 			<h1 className=" font-bold uppercase text-theme-primary text-base">
 				<span className="block lg:inline">{copy.name}</span>
 				<span className="hidden lg:inline text-gray-400" aria-hidden="true">
