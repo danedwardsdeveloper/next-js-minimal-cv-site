9638ea05b172ea129eeea6b927a39c6ea798180f
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-18 14:06:59 +0300

    Update styles, use theme colours

--- app/_components/LightDarkButton.tsx
+++ app/_components/LightDarkButton.tsx
@@ -1,17 +1,13 @@
 'use client';
 import { useTheme } from '../_contexts/ThemeContext';
 
-import { ThemeOptions } from '../_types/types';
-
 import Icon from './Icon';
 
 export default function LightDarkButton() {
 	const { theme, setTheme } = useTheme();
 
 	const toggleTheme = () => {
-		setTheme((prevTheme: ThemeOptions) =>
-			prevTheme === 'light' ? 'dark' : 'light'
-		);
+		setTheme(theme === 'light' ? 'dark' : 'light');
 	};
 
 	const currentIcon = theme === 'dark' ? 'sun' : 'moon';
@@ -20,10 +16,11 @@ export default function LightDarkButton() {
 		<button
 			type="button"
 			onClick={toggleTheme}
-			className=" items-center rounded-md bg-white dark:bg-gray-800 px-2 py-2  ring-gray-300  border-typescript  border-2 hover:bg-gray-200 dark:hover:bg-gray-700  hover:text-white transition-colors ease-in-out duration-300 shadow-lg"
+			className=" rounded-md   px-2 py-2  ring-typescript ring-offset-2 border-theme-secondary  border-2 transition-colors ease-in-out duration-300 hover:shadow-lg
+			bg-transparent hover:bg-theme-primary hover:bg-opacity-50"
 			aria-label={`Switch to ${theme === 'light' ? 'dark' : 'light'} mode`}
 		>
-			<Icon name={currentIcon} padding="p-0" colour="typescript" />
+			<Icon name={currentIcon} padding="p-0" colour="bg-theme-text" />
 		</button>
 	);
 }
