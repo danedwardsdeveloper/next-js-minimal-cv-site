83ad95e8ec306517b2301673023c55fe3f0bf389
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-31 23:21:44 +0100

    Render link as text for danedwardsdeveloper

--- app/_components/PortfolioPiece.tsx
+++ app/_components/PortfolioPiece.tsx
@@ -12,20 +12,31 @@ export default function PortfolioPiece({
 	details,
 }: PortfolioPieceProps) {
 	return (
-		<div className="mb-10">
-			<h3 className="font-bold uppercase mt-3 mb-1">
-				<Link
-					href={link}
-					className="text-theme-primary hover:text-theme-secondary hover:scale-105 transition-all duration-300"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					{name}
-				</Link>
+		<div className="mb-10" data-testid={`Portfolio piece ${name}`}>
+			<h3 className="font-bold uppercase mt-3 mb-1 flex items-center">
+				{link.includes('danedwardsdeveloper.com') ? (
+					<span
+						className="text-theme-primary"
+						title="You're already on the site"
+					>
+						{name}
+					</span>
+				) : (
+					<Link
+						href={link}
+						title={`Open ${link} in a new tab`}
+						className="text-theme-primary hover:scale-105 transition-all duration-300"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						{name}
+					</Link>
+				)}
 				<span className="text-theme-secondary px-3">|</span>
 				<Link
 					href={githubLink}
-					className="text-theme-primary hover:text-theme-secondary hover:scale-105 transition-all duration-300"
+					title={`Open ${githubLink} in a new tab`}
+					className="text-theme-primary hover:scale-105 transition-all duration-300"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
