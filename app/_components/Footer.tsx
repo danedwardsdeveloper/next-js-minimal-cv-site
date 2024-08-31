9f0117c5be368cc0a0efbfdda5e5777ed3783194
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-31 16:23:59 +0100

    Comment out GeneratePDF button

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,11 +1,13 @@
 import SimpleAnalyticsBadge from './SimpleAnalyticsBadge';
 
 import { copy } from '../_library/copy';
+// import GeneratePDFButton from './GeneratePdfButton';
 
 export default function Footer() {
 	return (
 		<footer className="w-full col-span-full flex-col-reverse md:flex-row gap-4 md:gap-0 md:row-start-2 flex justify-between md:items-end mb-10">
 			<SimpleAnalyticsBadge />
+			{/* <GeneratePDFButton /> */}
 			<p className="text-xs">{copy.copyright}</p>
 		</footer>
 	);
