7214807f093fd9b2f6fb7185d9350c0fa97e16e1
Dan Edwards <daniel.edwards96@yahoo.com>
2024-09-01 12:20:42 +0100

    Create words.ts

---
+++ app/__test_/utilities/words.ts
@@ -0,0 +1,37 @@
+export const spellCheckIgnoreList = [
+	'specialising',
+	'Optimised',
+	'optimisation',
+	'Recognised',
+	'Accessible',
+	'containerization',
+	'Axios',
+	'RESTful',
+	'serverless',
+	'organisational',
+	'WordPress',
+	'jQuery',
+	'Pixelmator',
+	'Canva',
+	'Metacritic',
+];
+
+export const commonWords = new Set([
+	'development',
+	'api',
+	'&',
+	'and',
+	'or',
+	'the',
+	'a',
+	'an',
+	'in',
+	'on',
+	'at',
+	'to',
+	'for',
+	'of',
+	'with',
+]);
+
+export const multiWordTerms = ['custom wordpress', 'headless wordpress'];
