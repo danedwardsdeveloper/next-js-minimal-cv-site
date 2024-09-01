0153bcf358a7caa4319dab24899ef0398c235419
Dan Edwards <daniel.edwards96@yahoo.com>
2024-09-01 12:21:05 +0100

    Add data-testid

--- app/_components/SkillList.tsx
+++ app/_components/SkillList.tsx
@@ -8,13 +8,20 @@ interface SkillListProps {
 export default function SkillList({ title, skills }: SkillListProps) {
 	return (
 		<section className="mb-14">
-			<h2 className="mb-3 font-bold uppercase text-theme-primary">
+			<h2
+				className="mb-3 font-bold uppercase text-theme-primary"
+				data-testid={`skills-list-${title}`}
+			>
 				{title}
 			</h2>
 
 			<ul>
 				{skills.map((skill, index) => (
-					<li key={index} className="mb-3">
+					<li
+						key={index}
+						className="mb-3"
+						data-testid={`${title}-skill-list-item-${index}`}
+					>
 						<div className="flex justify-items-start">
 							<Icon name="checkCircle" colour="text-theme-primary" />
 							<span>{skill}</span>
